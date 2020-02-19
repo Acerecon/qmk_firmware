e9bf55a0b8abad022bb56a7ff000b2e352ffa4c6
Acerecon <jessecollinsofficial@gmail.com>
2020-02-19 00:06:47 -0800

    Update keymap.c

--- keyboards/acerecon/aergo/keymaps/default/keymap.c
+++ keyboards/acerecon/aergo/keymaps/default/keymap.c
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB,   KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,  KC_BSPC,    KC_PGUP, KC_Y,  KC_U,  KC_I,    KC_O,    KC_P,    KC_DEL,
 		KC_LCTRL, KC_A,  KC_S,  KC_D,  KC_F,  KC_G,  KC_DEL,     KC_PGDN, KC_H,  KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT,
 		KC_LSFT,  KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,                       KC_N,  KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-		KC_LCTRL, 		       KC_LALT,       KC_SPC,             MO(1),    KC_ENT,     MO(2),                      KC_RCTRL
+		KC_LGUI, 		     KC_LALT,       KC_SPC,             MO(1),    KC_ENT,     MO(2),                      KC_RCTRL
     ),
 
     // FN1 Layer
@@ -46,8 +46,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		          KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS,            KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,
 		KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,           KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_BSPC,
 		KC_TRNS,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,  KC_TRNS,    KC_TRNS,  KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,  KC_TRNS,
-		KC_TRNS,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_TRNS,    KC_TRNS,  KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_TRNS,
-		KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_SPC,   KC_TRNS,                        KC_LBRC,  KC_RBRC,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+		KC_CAPSLOCK, KC_1,  KC_2,     KC_3,     KC_4,     KC_5,     KC_TRNS,    KC_TRNS,  KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_GRAVE,
+		KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_SPC,   KC_TRNS,                        KC_LBRC,  KC_RBRC,  KC_MINS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
 		KC_TRNS, 		       KC_TRNS,       KC_TRNS,                        KC_TRNS,    KC_TRNS,    KC_TRNS,                               KC_TRNS
     ),
 
